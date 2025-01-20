0258cfb68d0f803789093ebfe627b642c4b032a6
pan-ti <pandeandromeda@gmail.com>
2025-01-20 16:50:39 -0300

    criação da pasta componemts, e login para estilizaçao de background e formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Login from './Components/Login/Login';
 
 function App() {
 
 
   return (
     <div className="App">
-      <p>Página de Login</p>
+      <Login />
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='container'>
+        <form>
+            <h1>Acesse o Sistema</h1>
+            <input type="email" placeholder='Email'/>
+        </form>
+    </div>
+  )
+}
+
+export default Login
