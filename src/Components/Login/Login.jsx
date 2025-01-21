74d58b9217e18c0c4f9325da92a1a803c6174ed4
pan-ti <pandeandromeda@gmail.com>
2025-01-21 14:37:32 -0300

    Adicionado o componente Login com formulário funcional em React.Inclusão de funcionalidade para Lembre de mim e recuperação de senha.Configurção de  envio do formulário com validação básica via handleSubmit.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,19 +1,46 @@
-import {FaUser, FaLock} from 'react-icons/fa'
+import { FaUser, FaLock } from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./Login.css";
 
 const Login = () => {
+
+    const {username, setUsername} = useState('');
+    const {password, setPassword} = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Envio")
+    }
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o Sistema</h1>
         <div>
-          <input type="email" placeholder="Email" />
-        <FaUser className='icon'/>
+          <input type="email" placeholder="Email" onChange={(e) => setUsername(e.target.value)} />
+          <FaUser className="icon" />
         </div>
         <div>
           <input type="password" placeholder="Senha" />
-        <FaLock className='icon'/>
+          <FaLock className="icon" />
         </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueci minha senha</a>
+        </div>
+
         <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Cadastre-se</a>
+          </p>
+        </div>
       </form>
     </div>
   );
